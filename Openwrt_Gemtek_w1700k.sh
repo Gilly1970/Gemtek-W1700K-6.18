484369b33e82f3ba6b7138ed8c2823cbdac20d92
Gilly1970 <gilroyscott@hotmail.com>
2026-07-12 13:25:09 +1000

    Update commit

--- Openwrt_Gemtek_w1700k.sh
+++ Openwrt_Gemtek_w1700k.sh
@@ -31,7 +31,7 @@ readonly OPENWRT_REPO="https://github.com/openwrt/openwrt.git"
 #readonly OPENWRT_REPO="/home/user/openwrt/repos/openwrt"
 
 OPENWRT_BRANCH="master"
-readonly OPENWRT_COMMIT="25ee12629edcc38feffbd06255dd47840cd7af7e"
+readonly OPENWRT_COMMIT="7ff96bf05c502a5bedb2c89536c4df3e179a03fa"
 
 # --- Directory and File Configuration ---
 readonly SOURCE_DEFAULT_CONFIG_DIR="config"
